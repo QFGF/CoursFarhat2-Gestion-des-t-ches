f60b81d0d0c96ae9b02e90c29bc8c55a22c341e3
QFGF <50772649+QFGF@users.noreply.github.com>
2024-05-14 15:44:15 +0200

    Add files via upload

---
+++ projetfarhat2/Common/Tache.cs
@@ -0,0 +1,20 @@
+﻿namespace projetfarhat2
+{
+    //Création d'une Tache par default 
+    public class Tache
+    {
+        public string IdTache { get; set; } = default!;
+        public string NomTache { get; set; } = default!;
+        public string DescriptionTache { get; set; } = default!;
+        public string DateFinTache { get; set; } = default!;
+
+
+        public Tache(string idTache, string nomTache, string descriptionTache, string dateFinTache)
+        {
+            IdTache = idTache;
+            NomTache = nomTache;
+            DescriptionTache = descriptionTache;
+            DateFinTache = dateFinTache;
+        }
+    }
+}
---
+++ projetfarhat2/Crud.cs
@@ -0,0 +1,31 @@
+﻿using projetfarhat2;
+using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Crud
+{
+	    public static void AddTache(string IdTache, string NomTache, string DescriptionTache, string DateFinTache)
+        {
+            Tache AddedTache = new Tache("T" + IdTache, NomTache, DescriptionTache, DateFinTache);
+
+            // Ajout de la nouvelle tâche à la liste
+            ListeTaches.ListTache.Add(AddedTache);
+        }
+
+        public static string ShowTache(string IdTache)
+        {
+            return "y";
+        }
+
+        public static string MajTache(string IdTache)
+        {
+            return "y";
+        }
+
+        public static string DeleteTache(string IdTache)
+        {
+            return "y";
+        }
+}
